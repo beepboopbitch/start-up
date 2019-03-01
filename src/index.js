82e49d32797d1ec8c5f3ae9b0fc9b8f872ed9470
Nayomi Morita <nayomi.morita@gmail.com>
2019-03-01 09:04:17 -0800

    add menu dropdown in js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './sass/styles.scss';
+
+$(document).ready(function() {
+  event.preventDefault();
+  $(".bottom").hide();
+  $("#menu").click(function(){
+    $(".bottom").toggle();
+    display: block;
+  });
+
+
+});
